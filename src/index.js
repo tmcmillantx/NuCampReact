e24409a16fec409d371fb2d8767c6ef4e456d110
Terry McMillan <tmcmillan@lipstickonapig.com>
2020-04-04 13:05:59 -0500

    First commit to save code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
+import 'typeface-lobster';
+import 'typeface-open-sans';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +15,7 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+
