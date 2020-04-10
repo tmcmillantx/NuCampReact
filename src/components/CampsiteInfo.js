89307854cce3e4ec10265619c9423b3fa792f60c
Terry McMillan <tmcmillan@lipstickonapig.com>
2020-04-09 19:37:53 -0500

    Fetch from Server

--- src/components/CampsiteInfo.js
+++ src/components/CampsiteInfo.js
@@ -3,6 +3,7 @@ import { Button, Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, 
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './Loading';
+import { baseUrl } from '../shared/baseUrl';
 
 
 const required = val => val && val.length;
@@ -13,7 +14,7 @@ function RenderCampsite({campsite}) {
     return (
         <div className="col-md-5 m-1">
             <Card>
-                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
                 <CardBody>
                     <CardText>{campsite.description}</CardText>
                 </CardBody>
