e8d5432d7bfbff31ed47cf198425dedaa39a4592
Terry McMillan <tmcmillan@lipstickonapig.com>
2020-04-09 20:29:52 -0500

    Fetch Post Comment

--- src/components/CampsiteInfo.js
+++ src/components/CampsiteInfo.js
@@ -35,7 +35,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                     </div>
                     )
                 }
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={addComment} />
             </div>
         );
     }
@@ -62,7 +62,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     toggleModal() {
@@ -169,7 +169,7 @@ function CampsiteInfo(props) {
                 <RenderCampsite campsite={props.campsite} />
                 <RenderComments 
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                 />  
             </div>
