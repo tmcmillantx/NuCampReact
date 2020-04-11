fcef24b29c3f5fb23a97a1ed4dcfeed2aa45c92b
Terry McMillan <tmcmillan@lipstickonapig.com>
2020-04-10 19:22:57 -0500

    Corrected missing import

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,6 +10,7 @@ import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { actions } from 'react-redux-form';
 import { postComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 
 const mapStateToProps = state => {
